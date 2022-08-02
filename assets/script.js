8508f34acca3cf052e217980e961ee1e823649cf
kwlucas <lucaswkyle@gmail.com>
2022-08-01 20:25:32 -0600

    Base article search/fetch function

--- assets/script.js
+++ assets/script.js
@@ -5,12 +5,13 @@ var nameEl = document.querySelector("#coinName");
 var symbolEl = document.querySelector("#coinSymbol");
 var priceEl = document.querySelector("#coinPrice");
 
+var articleDispEl = document.querySelector("#headLineDisp");
+
 
 function searchHandler(event) {
     event.preventDefault();
     //get user input for search
     var searchInput = document.querySelector("#searchInput").value;
-    console.log(searchInput);
     //TODO: Validate search input
 
     searchCoin(searchInput);
@@ -33,19 +34,37 @@ function searchCoin(query) {
     fetch(`https://api.coingecko.com/api/v3/search?query=${query}`)
         .then(function (response) {
             //TODO Error check and throw
-            //console.log(response);
             return response.json();
         }).then(function (coinRes) {
             //TODO Narrow search results to most relevant b/c they are ordered by market cap
             coinRes = coinRes.coins;
-            //console.log(coinRes);
             var coinName = coinRes[0].name;
-            //TODO getArticles(coinName);
+            searchNews(coinName);
             //set text on html
             nameEl.textContent = coinName;
             symbolEl.textContent = coinRes[0].symbol;
             fetchPrice(coinRes[0].id);
         });
+}
+
+function searchNews(searchTerm){
+    //SHOULD NEVER PUT API KEYS IN PUBLIC REPO BUT IT IS HERE UNTIL WE GO OVER HOW TO HIDE IT
+    const apiKey = 'z2iUbTAjy0yD8xl2bqwIwJ2QgxfMRqZAvStnbuDk';
+    fetch(`https://api.thenewsapi.com/v1/news/all?api_token=${apiKey}&language=en&search=${searchTerm}`)
+    .then(function (response) {
+        return response.json();
+    }).then(function (articleRes){
+        articleRes = articleRes['data'];
+        articleRes.forEach(article => {//get info from each result
+            var artTitle = article.title
+            var artURL = article.url;
+            var artDesc = article.description;
+            var artDate = article.published_at;
+            console.log(`Title: ${artTitle}\nURL: ${artURL}\nDescription: ${artDesc}\nDate published: ${artDate}`);
+            //TODO write results to HTML
+        });
+    });
+    
 }
 
 searchButtonEl.addEventListener('click', searchHandler);
